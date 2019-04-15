a687f7a0f7cbf0204c13e2279cfaeed91d877307
Mathias Rav <m@git.strova.dk>
2019-04-15 12:57:35 +0200

    Extract state

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,18 @@ pub const DICE_COUNT: usize = 6;
 pub const BONUS_LIMIT: u32 = 4 * (1 + 2 + 3 + 4 + 5 + 6);
 pub const BONUS: u32 = 50;
 pub const REROLL_COUNT: usize = 2;
+
+pub type Comb = usize;
+
+pub const S2: Comb = 0;
+pub const S22: Comb = 1;
+pub const S222: Comb = 2;
+pub const S3: Comb = 3;
+pub const S4: Comb = 4;
+pub const S33: Comb = 5;
+pub const R15: Comb = 6;
+pub const R26: Comb = 7;
+pub const R16: Comb = 8;
+pub const S23: Comb = 9;
+pub const CHANCE: Comb = 10;
+pub const YAHTZEE: Comb = 11;
